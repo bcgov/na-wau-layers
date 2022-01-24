aa8b13baa71eaf3f4c28e5b30209df3a56ec78e8
KarHarker <karly.harker@gov.bc.ca>
2022-01-24 11:54:40 -0800

    formatting updates to new files

--- 06_scenario.R
+++ 06_scenario.R
@@ -10,3 +10,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+
+
+### EXTRA CODE ##### For combining columns for future scenarios
+mutate(Rank=case_when(
+  (Low>0 & Low<=3) & Medium==0 & High==0 & VHigh==0 ~ 'Low',
+  (Low>=0 & Medium>0 & Medium<=2 & High==0 & VHigh==0) | (Low>=4 & Medium==2 & High==0 & VHigh==0) ~ 'Medium',
+  (Low>=0 & Medium>=0 & High==1 & VHigh==0) | (Low>=0 & Medium>=3 & High==0 & VHigh==0) |
+    (Low==2 & Medium==2 & High==0 & VHigh==0) | (Low>=3 & Medium ==1 & High==0 & VHigh==0) ~ 'High',
+  (Low>=0 & Medium>=0 & High>=0 & VHigh>=1) | (Low>=0 & Medium>=2 & High==1 & VHigh==0) |
+    (Low>=0 & Medium>=0 & High>=2 & VHigh==0) ~ 'VHigh',
+  (Low==0 & Medium==0 & High==0 & VHigh==0 ~ 'Negligible' )
+))
